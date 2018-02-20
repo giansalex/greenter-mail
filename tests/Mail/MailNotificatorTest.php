a7c0203d57d0fc9e273deaaca05544a36cfd117c
giansalex <giansalex@gmail.com>
2018-02-20 18:00:20 -0500

    add unit test

--- tests/Mail/MailNotificatorTest.php
+++ tests/Mail/MailNotificatorTest.php
@@ -8,8 +8,17 @@
 
 namespace Tests\Greenter\Mail;
 
+use Greenter\Mail\MailNotificator;
+use Greenter\Notify\Notification;
 
-class MailNotificatorTest
+class MailNotificatorTest extends \PHPUnit_Framework_TestCase
 {
+    public function testNotification()
+    {
+        $mail = new MailNotificator();
 
+        $mail->notify(new Notification());
+
+        $this->assertTrue(true);
+    }
 }
