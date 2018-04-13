3ff8ffb484a6051f3421e4ca6520604dda2d5b21
jc <jzcastle@gmail.com>
2018-04-13 17:48:42 -0500

    Se añadio la libreria PHPMailer, y la plantilla, primera prueba OK

--- src/Mail/MailNotificator.php
+++ src/Mail/MailNotificator.php
@@ -11,16 +11,44 @@ namespace Greenter\Mail;
 use Greenter\Notify\Notification;
 use Greenter\Notify\NotificatorInterface;
 
-class MailNotificator implements NotificatorInterface
-{
+use Greenter\Mail\MailServer;
+use Greenter\Mail\MailSender;
+
+class MailNotificator implements NotificatorInterface {
+
+    private $mailServer;
+
+    public function __construct(MailServer $mailServer){
+        $this->mailServer = $mailServer;
+    }
+
     /**
      * @param Notification $notification
      * @param array $options
      *
      * @return mixed
      */
-    public function notify(Notification $notification, $options = [])
-    {
-        // TODO: Implement notify() method.
+    public function notify(Notification $notification, $options = []){
+        
+        $data = [
+            'cliente' => 'CLIENTE SA',
+            'tipo_documento' => "Factura Electrónica",
+            'numero' => 'F001-1',
+            'emision' => '2018-04-13',
+            'vencimiento' => '2018-04-13',
+            'moneda' => 'SOLES',
+            'monto' => '100.00',
+            'tipo' => '01',
+            'uuid' => '',
+            'es_gratuito' => false
+        ];
+
+        $this->mailServer->setBody($this->getTemplate($data));
+        $this->mailServer->send();
+    }
+
+    private function getTemplate($data){
+        $twig = new \Twig_Environment(new \Twig_Loader_Filesystem(__DIR__ . '/Templates'));
+        return $twig->render('mail.html.twig', $data);
     }
 }
--- tests/Mail/MailNotificatorTest.php
+++ tests/Mail/MailNotificatorTest.php
@@ -11,14 +11,37 @@ namespace Tests\Greenter\Mail;
 use Greenter\Mail\MailNotificator;
 use Greenter\Notify\Notification;
 
-class MailNotificatorTest extends \PHPUnit_Framework_TestCase
-{
-    public function testNotification()
-    {
-        $mail = new MailNotificator();
+use Greenter\Mail\MailServer;
+use Greenter\Mail\MailEmail;
+use Greenter\Mail\Config\ConfigGmail;
+
+class MailNotificatorTest extends \PHPUnit_Framework_TestCase{
+
+    public function testNotification(){
+
+    	$options = [
+    		'SMTPDebug' => 2,
+    		'isSMTP' => true,
+    		'Host' => 'smtp.gmail.com',
+    		'SMTPAuth' => true,
+    		'Username' => 'correo',
+    		'Password' => 'clave',
+    		'SMTPSecure' => 'tls',
+    		'Port' => 587
+    	];
+
+    	$mailServer = new MailServer($options, MailServer::DEBUG);
+        $mailServer->setSender(new MailEmail('correo', 'CORREO PRUEBA'));
+        $mailServer->setSubject('Documento electrónico emitido por Nombre Empresa SAC');
+        $mailServer->setReceipt(new MailEmail('Appee1975@dayrep.com', 'CORREO PRUEBA')); //fakemailgenerator.com
+
+        $mail = new MailNotificator($mailServer);
 
         $mail->notify(new Notification());
 
         $this->assertTrue(true);
     }
 }
+
+/*$test = new MailNotificatorTest();
+$test->testNotification();*/
